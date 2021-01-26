d2285b4b4e753313fcf1e52ec5711b18fa381f05
saikiran_s <saikiran@gifskey.com.com>
2021-01-27 01:58:57 +0530

    updated accessibility

--- src/components/shared/Footer/index.vue
+++ src/components/shared/Footer/index.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="footer-links">
       <div class="container">
-        <g-link to="https://github.com/saikiran-siriki" target="_blank" rel="noopener"><font-awesome-icon :icon="['fab', 'github']" /></g-link>
-        <g-link to="https://stackoverflow.com/users/8994551/saikiran" target="_blank" rel="noopener"><font-awesome-icon :icon="['fab', 'stack-overflow']" /></g-link>
-        <g-link to="https://www.linkedin.com/in/saikiran-siriki-296a22121/" target="_blank" rel="noopener"><font-awesome-icon :icon="['fab', 'linkedin']" /></g-link>
+        <g-link aria-label="Github" to="https://github.com/saikiran-siriki" target="_blank" rel="noopener"><font-awesome-icon :icon="['fab', 'github']" /></g-link>
+        <g-link aria-label="Stackoverflow" to="https://stackoverflow.com/users/8994551/saikiran" target="_blank" rel="noopener"><font-awesome-icon :icon="['fab', 'stack-overflow']" /></g-link>
+        <g-link aria-label="Linkedin" to="https://www.linkedin.com/in/saikiran-siriki-296a22121/" target="_blank" rel="noopener"><font-awesome-icon :icon="['fab', 'linkedin']" /></g-link>
       </div>
       
     </div>
