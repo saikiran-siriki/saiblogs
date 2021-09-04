be8d8769a2d027c06a42c248b06228d3a5b9489d
saikiran_siriki <sirikisaikiran@gmail.com>
2021-09-04 13:12:52 +0530

    updated the code base to be rendered purely based on config

--- src/components/shared/Footer/index.vue
+++ src/components/shared/Footer/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="footer-links">
+    <div class="footer">
       <div class="container">
         <g-link class="footer-link" aria-label="Github"  to="https://github.com/saikiran-siriki" target="_blank" rel="noopener"><font-awesome-icon :icon="['fab', 'github']" /></g-link>
         <g-link class="footer-link" aria-label="Stackoverflow" to="https://stackoverflow.com/users/8994551/saikiran" target="_blank" rel="noopener"><font-awesome-icon :icon="['fab', 'stack-overflow']" /></g-link>
---
+++ src/pages/blogs/BlogTemplate.vue
@@ -0,0 +1,41 @@
+<template>
+  <Layout class="layout">
+    <div class="layout-inner">
+      <headersection />
+      <div class="blogs-section container">
+        <blogcontent :blogdata="blogdata" />
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import headersection from "@/components/Home/Header";
+import blogcontent from "@/components/Blogs/BlogContent";
+import blogsArr from '@/config/blogs'
+export default {
+  components: { headersection, blogcontent },
+  metaInfo() {
+      return this.metaInfo
+  },
+  data() {
+    return {
+      blogdata: {},
+      metaInfo: {}
+    };
+  },
+  created(){
+      blogsArr.forEach((item)=>{
+          if(this.$route.params.routename===item.blogUrl){
+              this.blogdata = item
+              this.metaInfo = item.metaInfo
+          }
+      })
+  }
+};
+</script>
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
+
+
